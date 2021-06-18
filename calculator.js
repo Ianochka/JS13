4742eaf190436e4eaba1bcf62934a66045177e11
Ianochka <iana.a.barabash@gmail.com>
2021-06-18 16:47:53 +0200

    Простой калькулятор

---
+++ calculator.js
@@ -0,0 +1,27 @@
+function showMessage1() {
+    let a = prompt(`First number:`);
+    let b = prompt(`Second number:`);
+    let c = result = Number(a) + Number(b);
+    alert(`Result: ${result}`);
+}
+
+function showMessage2() {
+    let a = prompt(`First number:`);
+    let b = prompt(`Second number:`);
+    let c = result = Number(a) - Number(b);
+    alert(`Result: ${result}`);
+}
+
+function showMessage3() {
+    let a = prompt(`First number:`);
+    let b = prompt(`Second number:`);
+    let c = result = Number(a) / Number(b);
+    alert(`Result: ${result}`);
+}
+
+function showMessage4() {
+    let a = prompt(`First number:`);
+    let b = prompt(`Second number:`);
+    let c = result = Number(a) * Number(b);
+    alert(`Result: ${result}`);
+}
